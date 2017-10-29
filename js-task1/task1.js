6f8ff44860ec62c207919826be15fe71e70410ef
812271639 <812271639@qq.com>
2017-10-30 00:00:34 +0800

    my

--- js-task1/task1.js
+++ js-task1/task1.js
@@ -11,18 +11,19 @@ function start() {
     for (var b = 0; b < 9; b++) {
         change[b].style.background = "#fea500";
     }
-    //选择三个不同的随机数。如果都不相同则break跳出循环，执行函数。如果相同，则继续执行循环
-    while(true) {
-        var z = Math.floor(Math.random() * 9);
-        var y = Math.floor(Math.random() * 9);
-        var x = Math.floor(Math.random() * 9);
-        if(z !== y && z !== x && x !== y) {
-            break;
-        }
+    var arr = new Array(9);
+    for (var i=0; i<9; i++) {
+        arr[i] = i;
     }
-    change[z].style.background = colors();
-    change[y].style.background = colors();
-    change[x].style.background = colors();
+    var arr2 = new Array();
+    for (var i=9; i>0; i--) {
+        var rnd = Math.floor(Math.random()*i);
+        arr2.push(arr[rnd]);
+        arr.splice(rnd,1);
+    }
+    change[arr2[0]].style.background = colors();
+     change[arr2[1]].style.background = colors();
+    change[arr2[2]].style.background = colors();
 }
 //生成随机颜色
 function colors() {
@@ -38,7 +39,7 @@ function colors() {
 var mytime;                                  //放在函数里不起作用？
 function myFunction() {
     clearInterval( mytime);                  //清除定时
-    mytime = setInterval(start, 1000);       //连续执行定时
+    mytime = setInterval(start, 1500);       //连续执行定时
     // setInterval('start()', 1000);
     // setInterval(function(){ start() }, 1000);
 
@@ -54,6 +55,20 @@ function end() {
 
 }
 
+//选择三个不同的随机数。如果都不相同则break跳出循环，执行函数。如果相同，则继续执行循环
+// while(true) {
+//     var z = Math.floor(Math.random() * 9);
+//     var y = Math.floor(Math.random() * 9);
+//     var x = Math.floor(Math.random() * 9);
+//     if(z !== y && z !== x && x !== y) {
+//         break;
+//     }
+// }
+//     change[z].style.background = colors();
+//     change[y].style.background = colors();
+//     change[x].style.background = colors();
+
+
 
 // function start() {
 //     var change = document.getElementsByClassName('content');
@@ -64,20 +79,9 @@ function end() {
 //         change[hello(8)].style.background = colors() ;
 //     }
 // }
-// function hello(m)
-// {
-//     var arr = new Array(m);
-//     for (var i=0; i<m; i++) {
-//         arr[i] = i;
-//     }
-//     var arr2 = new Array();
-//     for (var i=m; i>0; i--) {
-//         var rnd = Math.floor(Math.random()*i);
-//         arr2.push(arr[rnd]);
-//         arr.splice(rnd,1);
-//      }
-//     return arr2[1];
-//  }
+
+
+//
 // function shuffle_pick_1(m)
 // {
 //     var arr = new Array(m);
@@ -93,4 +97,4 @@ function end() {
 //     return arr2[i];
 // }
 //
-//
+
