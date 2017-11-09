8966d7174f42a929ba4ae8aed92536180fc5a22a
812271639 <812271639@qq.com>
2017-11-09 17:44:17 +0800

    hello

--- js-task1/task1.js
+++ js-task1/task1.js
@@ -4,11 +4,11 @@ function start() {
     for (var b = 0; b < 9; b++) {
         change[b].style.background = "#fea500";
     }
-    var arr = new Array(9);
+    var arr = new Array(9);                        //生成数组
     for (var i = 0; i < 9; i++) {
         arr[i] = i;
     }
-    var arr2 = new Array();
+    var arr2 = new Array();                        //打乱数组1，推送到数组2
         for (var i = 9; i > 0; i--) {
             var rnd = Math.floor(Math.random() * i);
             arr2.push(arr[rnd]);
@@ -29,16 +29,16 @@ function colors() {
     }
 }
 
-
-var mytime;                                  //放在函数里不起作用？
+var mytime; //放在函数里不起作用？（变量初始化，将定时器清除了.
+            // 每次执行函数都会从新定义这个变量，这个变量没有初始化，
+            // clearInterval(mytime);就没有意义。放在函数外面，当执行一次函数后，
+            // 这个变量就初始化为mytime = setInterval(start, 1000); 。从而保存了一个值。后面函数执行就可以调用)
 function myFunction() {
-    clearInterval(mytime);                  //清除定时
+    clearInterval(mytime);         //清除定时
     mytime = setInterval(start, 1000);       //连续执行定时
     // setInterval('start()', 1000);
     // setInterval(function(){ start() }, 1000);
-
 }
-
 //结束定时
 function end() {
     clearInterval(mytime);                     //清除定时
@@ -47,9 +47,8 @@ function end() {
     for (i = 0; i < color.length; i++) {
         color[i].style.background = '';
     }
-
 }
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //选择三个不同的随机数。如果都不相同则break跳出循环，执行函数。如果相同，则继续执行循环
 // while(true) {
@@ -93,7 +92,7 @@ function end() {
 // }
 //
 
-/////////////////////////////////////关于函数内部 的函数 的 执行问题//////////////////////////////////////
+/////////////////////////////////////关于函数内部 的函数 的 执行问题///////////////////////////////////////////////////////
 // function start() {
 //     var change = document.getElementsByClassName('content');//在每次定时开始前回复初始状态
 //     for (var b = 0; b < 9; b++) {
