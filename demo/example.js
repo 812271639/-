1df38d003cbb54aff1c46597519a40b162e59960
812271639 <812271639@qq.com>
2017-11-15 19:11:31 +0800

    hello

--- demo/example.js
+++ demo/example.js
@@ -22,85 +22,274 @@
 // hello(say);
 
 var fsm = new StateMachine({
-    init: 'solid',
+    init: 'none',
     transitions: [
-        {name: 'melt', from: 'solid', to: 'liquid1'},
-        {name: 'vaporize', from: 'liquid1', to: 'gas'},
-        {name: 'condense', from: 'gas', to: 'liquid'},
-        {name: 'freeze', from: 'liquid', to: 'solid'}
+        {name:'start' , from:'none',to:'A'},
+        {name: 'Melt', from: 'A', to: 'B'},
+        {name: 'Vaporize', from: 'B', to: 'C'},
+        {name: 'Condense', from: 'C', to: 'D'},
+        {name: 'Freeze', from: 'D', to: 'E'},
+        {name: 'test', from: 'E', to: 'F'},
+        {name:'clear',from:'F',to:'none'}
+        // {
+        //     name: 'goto', from: '*', to: function (a) {
+        //     return a
+        // }
+        // } //q 可以是任何字符,数字不可以
     ],
+    // data: {
+    //     color: 'red',
+    //     girl: '小姐姐'
+    // },
 
     methods: {
-        onBeforeMelt: function () {        //name: 'melt', from: 'solid', to: 'liquid'
-            console.log('在融化前111')
+        
+        // hello:function () {
+        //     console.log('另一个小姐姐')
+        // },
+        // onMelt:     function() { console.log(' melted to ' + fsm.state);  },
+        // onVaporize: function() { console.log(' vaporized to ' + fsm.state) },
+        // onCondense: function() { console.log(' condensed to ' + fsm.state) },
+        // onFreeze:   function() { console.log(' froze to ' + fsm.state)     }
+        //
+        onBeforeMelt: function (lifecycle, arg1, arg2) {
+            console.log("name: 'melt', from: 'A', to: 'B'");
+            // console.log('事件： ' + lifecycle.transition);
+            // console.log('离开 ' + lifecycle.from + '状态');
+            // console.log('进入 ' + lifecycle.to + '状态');
+            // console.log(arg1);
+            // console.log(arg2);
+            console.log('1111 在事件 melt 前,当前状态 ' + fsm.state);
+
         },
-        onLeaveSolid: function () {
-            console.log('离开固体时111')
+        onLeaveA: function () {
+            console.log('11112执行事件melt，离开 A 状态,当前状态 ' + fsm.state);
         },
-        onEnterLiquid1: function () {
-            console.log('进入液体时111')
+        onEnterB: function () {
+            console.log('11113执行事件melt，进入 B 状态,当前状态 ' + fsm.state);
         },
         onAfterMelt: function () {
-            console.log('在融化后111')
+            console.log('11114在事件 melt 后,当前状态 ' + fsm.state);
+            console.log();
         },
+        onBeforeVaporize: function () {
+            console.log("name: 'vaporize', from: 'B', to: 'C'");
 
-
-
-        onBeforeVaporize: function () {    //name: 'vaporize', from: 'liquid', to: 'gas'
-            console.log('在蒸发前222')
+            console.log('22221在事件Vaporize前,当前状态 ' + fsm.state);
         },
-        onLeaveLiquid1: function () {
-            console.log('离开液体时222')
+        onLeaveB: function () {
+
+            console.log('22222执行事件Vaporize，离开B，当前状态 ' + fsm.state);
         },
-        onEnterGas: function () {
-            console.log('进入气体时222')
+        onEnterC: function () {
+            console.log('22223执行事件Vaporize，进入C，当前状态 ' + fsm.state);
         },
         onAfterVaporize: function () {
-            console.log('在蒸发后222')
+            console.log('22224在事件Vaporize后，当前状态 ' + fsm.state);
         },
-
-
-        onBeforeCondense: function () {     //name: 'condense', from: 'gas', to: 'liquid'
-            console.log('在凝结前333')
+        onBeforeCondense: function () {
+            console.log("name: 'condense', from: 'C', to: 'D'");
+            console.log('33331在事件Condense前，当前状态 ' + fsm.state);
         },
-        onLeaveGas: function () {
-            console.log('离开气体时333')
+        onLeaveC: function () {
+            console.log('33332执行事件Condense，离开C，当前状态 ' + fsm.state);
         },
-        onEnterLiquid: function () {
-            console.log('进入液体时333')
+        onEnterD: function () {
+            console.log('33333执行事件Condense，进入D，当前状态 ' + fsm.state);
         },
         onAfterCondense: function () {
-            console.log('在蒸发后333')
+            console.log('33334在事件Condense后，当前状态 ' + fsm.state);
         },
+        onBeforeFreeze: function () {
+            console.log("name: 'freeze', from: 'D', to: 'E'");
+            console.log('44441在事件Freeze前，当前状态 ' + fsm.state);
+        },
+        onLeaveD: function () {
+            console.log('44442执行事件Freeze，离开D，当前状态 ' + fsm.state);
+        },
+        onEnterE: function () {
+            console.log('44442执行事件Freeze，进入E，当前状态 ' + fsm.state);
 
 
-
-        onBeforeFreeze: function () {   //name: 'freeze', from: 'liquid', to: 'solid'
-            console.log('在冻结前444')
-        },
-        onLeaveLiquid: function () {
-            console.log('离开液体时444')
-        },
-        onEnterSolid: function () {
-            console.log('进入固体时444')
         },
         onAfterFreeze: function () {
-            console.log('在冻结后444')
+
+            console.log('44444在事件Freeze后,当前状态 ' + fsm.state);
+        },
+        onLeaveE: function () {
+            console.log('离开E，当前状态 ' + fsm.state);
+        },
+        onEnterF: function () {
+            console.log('进入F，当前状态 ' + fsm.state);
+        },
+        onEnterNone: function () {
+            console.log('44442执行事件Freeze，进入none，当前状态 ' + fsm.state);
+
+
         }
-
-
-
-
-
+//         // onLeaveState:function () {
+//         //     return false; // 会终止后续转换onBeforeTransition  onBefore<TRANSITION>  onLeaveState  onLeave<STATE>  onTransition
+//         // }
     }
+
 });
-// fsm.state;
+
+// fsm.onMelt();             //状态都是初始状态
+// fsm.onFreeze();           //状态都是初始状态
+// fsm.onVaporize();         //状态都是初始状态
+// fsm.onCondense();         //状态都是初始状态
+// $(document).ready(function () {
+// var fsm = new StateMachine({
+//
+//     init: 'menu',
+//
+//     transitions: [
+//         { name: 'play1', from: 'menu', to: 'game' },
+//         { name: 'quit', from: 'game', to: 'menu' }
+//     ],
+//
+//     methods: {
+//         onLeaveMenu: function() {
+//
+//             return new Promise(function(resolve, reject) {
+//                 $('#menu').fadeOut(3000, resolve);
+//                 console.log('1离开Menu,当前状态 ' + fsm.state + '隐藏 小姐姐');
+//             })
+//
+//         },
+//         onEnterGame: function() {
+//             return new Promise(function(resolve, reject) {
+//                 $('#game').fadeIn(2000, resolve);
+//                 console.log('2进入Game,当前状态 '+ fsm.state + '显示 另一个小姐姐');
+//             })
+//         },
+//         onLeaveGame: function() {
+//             return new Promise(function(resolve, reject) {
+//                 $('#game').fadeOut(2000, resolve);
+//                 console.log('3离开Game,当前状态 '+fsm.state + '隐藏 另一个小姐姐');
+//             })
+//         },
+//         onEnterMenu: function() {
+//             return new Promise(function(resolve, reject) {
+//                 $('#menu').fadeIn('fast', resolve);
+//                 console.log('4进入Menu,当前状态 '+fsm.state + '显示 小姐姐');
+//             })
+//         }
+//     }
+// });
+
+
+// document.getElementById("ff").onclick = function () {
+//     fsm.play1();
+//
+// };
+// document.getElementById("gg").onclick = function () {
+//     fsm.quit();
+// };
+// });
+fsm.start();
+document.getElementById("aa").onclick = function () {
+    fsm.melt('小姐姐', '另一个小姐姐');
+};
+document.getElementById("bb").onclick = function () {
+    fsm.vaporize();
+};
+document.getElementById("cc").onclick = function () {
+    fsm.condense();
+};
+document.getElementById("dd").onclick = function () {
+    fsm.freeze();
+};
+document.getElementById("ee").onclick = function () {
+    console.log('goto前的状态 ' + fsm.state);
+    fsm.goto('C');
+    console.log('goto后的状态 ' + fsm.state);
+    // console.log(fsm.color);
+    // console.log(fsm.girl);
+    // fsm.hello();
+
+    // goto 用法比较奇怪，会执行两个状态变化 ？？？
+    // 首先是执行离开初始状态A的步骤，再进入执行相应状态（也就是说goto的状态也是由相应的状态转变而来）。
+    // 例如：goto状态为D，
+    // 首先（执行事件melt，离开 A 状态,当前状态 A），
+    // 接着（执行事件Condense，进入液体，当前状态 D）；
+    // goto状态为C
+    // 首先（执行事件melt，离开 A 状态,当前状态 A），
+    // 接着（执行事件Vaporize，进入C，当前状态 C）。
+};
+
+//     <button id="aa" onclick="fsm.melt()" >melt融化</button>
+//     <button id="bb" onclick="fsm.vaporize()" >vaporize汽化</button>
+//     <button id="cc"  >condense冷凝</button>
+//     <button id="dd" onclick="fsm.freeze()" >freeze冻结</button>
+//
+// var fsm = new StateMachine({
+//     init: 'A',
+//     transitions: [
+//         { name: 'melt',     from: 'A',  to: 'D' },
+//         { name: 'freeze',   from: 'D', to: 'A'  },
+//         { name: 'vaporize', from: 'D', to: 'C'    },
+//         { name: 'condense', from: 'C',    to: 'D' }
+//     ]
+// });
+//
+// fsm.state;             //当前 状态为 'A'  （转换前）
 // fsm.melt();
-// fsm.freeze();
+// fsm.state;             //当前 状态为  'D' （fsm.melt()转换后）
 // fsm.vaporize();
-// fsm.condense();
-// //
-// onMelt();
-// onFreeze();
-// onVaporize();
-// onCondense();
+// fsm.state;             //当前 状态为 'C'     （fsm.vaporize()转换后）
+
+
+// Observers will be passed a single argument containing a lifecycle object with the following attributes:
+//..观察者将传递一个包含lifecycle具有以下属性的对象的参数
+
+// transition - the transition name   过渡名称
+// from - the previous state          以前的状态
+// to - the next state                下一个状态
+
+//..它创建一个具有当前状态属性的对象：
+
+// fsm.state          （当前状态）
+
+//在转换的生命周期中自动调用的观察者方法：fsm.onStep() 如：
+//    fsm.onMelt()
+//    fsm.onFreeze()
+//    fsm.onVaporize()
+//    fsm.onCondense()
+
+//辅助方法：                                                 可用于条件判断
+// fsm.is(s) - 如果状态s是当前状态，则返回true
+// fsm.can(t) - 如果t从当前状态发生转换，则返回true
+// fsm.cannot(t) - 如果t从当前状态不能发生转换，则返回true
+// fsm.transitions() - 返回当前状态允许的转换列表
+// fsm.allTransitions() - 返回所有可能的转换的列表
+// fsm.allStates() - 返回所有可能状态的列表
+
+//转换的生命周期按以下顺序进行：
+//
+// onBeforeTransition - 在任何转换之前触发
+// onBefore<TRANSITION> - 在特定的转换之前触发
+// onLeaveState - 离开任何状态被触发
+// onLeave<STATE> - 离开特定的状态触发
+// onTransition - 在任何过渡期间被触发
+// onEnterState - 进入任何状态被触发
+// onEnter<STATE> - 进入特定状态触发
+// on<STATE> - 简写 onEnter<STATE>
+// onAfterTransition - 任何转换后触发
+// onAfter<TRANSITION> - 在特定的TRANSITION后触发
+// on<TRANSITION> - 简写 onAfter<TRANSITION>
+
+
+//Cancelling a Transition  取消转换
+//
+// Any observer can cancel a transition by explicitly returning false during any of the following lifecycle events:
+// 任何观察者都可以通过false在以下任何生命周期事件中显式返回来取消转换：
+//
+// onBeforeTransition
+// onBefore<TRANSITION>
+// onLeaveState
+// onLeave<STATE>
+// onTransition
+//
+// All subsequent lifecycle events will be cancelled and the state will remain unchanged.
+// 所有随后的生命周期事件将被取消，状态将保持不变。
--- js-task4/js/judge-diary.js
+++ js-task4/js/judge-diary.js
@@ -1,83 +1,77 @@
 
 
 $(document).ready(function () {
-    //
-    // var first_day_id = document.getElementById('first_day_id'),
-    //     kill   = document.getElementById('kill'),
-    //     lastWords = document.getElementById('lastWords'),
-    //     speak   = document.getElementById('speak'),
-    //     vote   = document.getElementById('vote');
 
    var fsm = new StateMachine({
-       init:        'ready',
+       init:      'none',
        transitions:[
-           // { name: 'start',from: 'none',to: 'die'},
-           {name:'kill',from:'die',to:'lastSpeak'},
-           {name:'lastWords',from:'lastSpeak',to:'discuss'},
-           {name:'speak',from:'discuss',to:'results'},
-           {name:'vote',from:'results',to:'none'}
+           {name:'kill',from:'none',to:'die'},
+           {name:'lastWords',from:'die',to:'lastSpeak'},
+           {name:'speak',from:'lastSpeak',to:'discuss'},
+           {name:'vote',from:'discuss',to:'results '}
        ],
        methods:{
-           onAfterTransition:function (lifecycle) {
-               $('#kill').css('background-color','blue');
-               // $('#lastWords').off('click',function () {
-               //     $(this).css('background-color','yellow');
-               // });
+           onBeforeKill:function (lifecycle){                     //{name:'kill',from:'none',to:'die'},
+               console.log('1111');
            },
-           onBeforeKill:function (lifecycle){
-               console.log('transition: ' + lifecycle.transition);
-               console.log('from: '       + lifecycle.from);
-               console.log('to: '         + lifecycle.to);
-               // console.log(zzzzs);
-               // $('#lastWords').click(alert('请先杀人'));
-               // $('#speak').click(alert('请先杀人'));
-               // $('#vote').click(alert('请先杀人'));
+           onLeaveNone:function (lifecycle) {
+               console.log('1111');
            },
-           onleavedie:function (lifecycle) {
+           onEnterDie:function (lifecycle) {
+               console.log('1111')
+           },
+           onAfterKill:function (lifecycle) {
+               console.log('1111');
 
            },
-           onenterlastSpeak:function (lifecycle) {
+///////////////////////////////////////////////////////////    {name:'lastWords',from:'die',to:'lastSpeak'},
+           onBeforeLastWords:function (lifecycle) {
+               console.log(2222);
 
            },
-           onAfterkill:function (lifecycle) {
-               console.log(12345);
-               $('#kill').css('background-color','yellow').off('click');
-               $('#lastWords').alert('请死者亮明身份并发言');
+           onLeaveDie:function (lifecycle) {
+               console.log(2222);
            },
-           onBeforelastWord:function (lifecycle) {
-               $('#speak').click(alert('请死者亮明身份并发言'));
-               $('#vote').click(alert('请死者亮明身份并发言'));
+           onEnterLastSpeak:function (lifecycle) {
+               console.log(2222);
            },
-           onleavelastSpeak:function (lifecycle) {
+           onAfterLastWords:function(lifecycle){
+               console.log(2222);
 
            },
-           onenterdiscuss:function (lifecycle) {
+//////////////////////////////////////////////////////////////   {name:'speak',from:'lastSpeak',to:'discuss'},
+           onBeforeSpeak:function (lifecycle) {
+               console.log(3333);
+           },
+           onLeaveLastSpeak:function (lifecycle) {
+               console.log(3333);
+           },
+           onEnterDiscuss:function (lifecycle) {
+               console.log(3333);
+           },
+           onAfterSpeak:function(lifecycle){
+               console.log(3333);
+           },
+//////////////////////////////////////////////////////////////    {name:'vote',from:'discuss',to:'results '}
+           onBeforeVote:function () {
+               console.log(4441);
+           },
+           onLeaveDiscuss:function () {
+               console.log(4442);
+           },
 
+           onEnterResults:function () {              //这一句是没有的（none）
+               console.log(4443);
            },
-           onAfterlastWord:function(lifecycle){
-               $('#lastWords').css('background-color','yellow');
-               $('#speak').click(alert('开始讨论'));
-               $('#vote').click(alert('请先开始讨论'));
-           },
-           onBeforespeak:function (lifecycle) {
 
-           },
-           onleavediscuss:function (lifecycle) {
-
-           },
-           onenterresults:function (lifecycle) {
-
-           },
-           onAfterspeak:function(lifecycle){
-               $('#speak').css('background-color','yellow');
+           onAfterVote:function () {
+               console.log(4444);
            }
        }
    });
 
     $('#kill').click(function () {
-        // console.log(232323);
         fsm.kill();
-        // console.log(fsm.kill);
         // $(this).css('color','red');
     });
     $('#lastWords').click(function () {
@@ -91,8 +85,5 @@ $(document).ready(function () {
     $('#vote').click(function () {
         fsm.vote();
     });
-    // fsm.state;
-    fsm.start();
-    fsm.kill();
-    return fsm;
+
 });
