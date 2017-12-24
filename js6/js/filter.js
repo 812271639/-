bd0e50f23a3adbb848c132837f72f6e5355c6a8d
812271639 <812271639@qq.com>
2017-12-24 15:25:13 +0800

    hello

--- js6/js/filter.js
+++ js6/js/filter.js
@@ -20,8 +20,6 @@ app.filter('typesFilter', function (types) {
     return function (input) {
         if (input !== "" && input !== undefined) {
             return types[input];
-        } else {
-            return "无";
         }
     }
 });
@@ -29,8 +27,6 @@ app.filter('stateFilter', function (states) {
     return function (input) {
         if (input !== "" && input !== undefined) {
             return states[input];
-        } else {
-            return "无";
         }
     }
 });
@@ -38,8 +34,6 @@ app.filter("putAwayFilter", function (putAway) {
     return function (input) {
         if (input !== "" && input !== undefined) {
             return putAway[input];
-        } else {
-            return "无";
         }
     }
 
